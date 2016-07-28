12f145166f43eb956637fe00aff326ad25553620
Ian Miell <ian.miell@gmail.com>
2016-07-28 15:31:33 +0100

    makefiles

--- build.sh
+++ build.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e
 set -x
-for d in $(find . -name diagrams)
-do
-	pushd $d
-	for f in $(ls -1 *mermaid)
-	do
-		make
-	done
-	popd
-done
 for f in $(find [0-9]* -maxdepth 0 -type d)
 do
 	pushd $f
